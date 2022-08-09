5161a98f628b8ea421d25380467a8998ecca9f6e
Robert Deskoski <rdeskoski@switch.tv>
2022-08-09 21:44:40 +1000

    Second push

---
+++ app/Http/Controllers/AnnouncementController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use App\Models\Announcement;
+use Illuminate\Support\Facades\Validator;
+
+class AnnouncementController extends Controller
+{
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        $announcements = Announcement::all();
+        return Inertia::render('Announcements/Index', ['announcements' => $announcements]);
+    }
+
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function create()
+    {
+        return Inertia::render('Announcements/Create');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        Validator::make($request->all(), [
+            'title' => ['required'],
+            'content' => ['required'],
+            'startDate' => 'nullable|date',
+            'endDate' => 'nullable|date',
+            'active' => ['required'],
+        ])->validate();
+
+        Announcement::create($request->all());
+
+        return redirect()->route('announcements.index');
+    }
+
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function edit(Announcement $announcement)
+    {
+        return Inertia::render('Announcements/Edit', [
+            'announcement' => $announcement
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function update($id, Request $request)
+    {
+        Validator::make($request->all(), [
+            'title' => ['required'],
+            'content' => ['required'],
+            'startDate' => 'nullable|date',
+            'endDate' => 'nullable|date',
+            'active' => ['required'],
+        ])->validate();
+
+        Announcement::find($id)->update($request->all());
+        return redirect()->route('announcements.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        Announcement::find($id)->delete();
+        return redirect()->route('announcements.index');
+    }
+}
---
+++ resources/js/Pages/Announcements/Create.jsx
@@ -0,0 +1,158 @@
+import React from "react";
+import Authenticated from "@/Layouts/Authenticated";
+import { Head, useForm, Link } from "@inertiajs/inertia-react";
+
+export default function Dashboard(props) {
+    const { data, setData, errors, announcement } = useForm({
+        title: "",
+        content: "",
+        startDate: "",
+        endDate: "",
+        active: "",
+    });
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        announcement(route("announcements.store"));
+    }
+
+    return (
+        <Authenticated
+            auth={props.auth}
+            errors={props.errors}
+            header={
+                <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+                    Create Announcement
+                </h2>
+            }
+        >
+            <Head title="Announcements" />
+
+            <div className="py-12">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div className="p-6 bg-white border-b border-gray-200">
+                            <div className="flex items-center justify-between mb-6">
+                                <Link
+                                    className="px-6 py-2 text-white bg-blue-500 rounded-md focus:outline-none"
+                                    href={route("announcements.index")}
+                                >
+                                    Back
+                                </Link>
+                            </div>
+
+                            <form name="createForm" onSubmit={handleSubmit}>
+                                <div className="flex flex-col">
+                                    <div className="mb-4">
+                                        <label className="">Title</label>
+                                        <input
+                                            type="text"
+                                            className="w-full px-4 py-2"
+                                            label="Title"
+                                            name="title"
+                                            value={data.title}
+                                            onChange={(e) =>
+                                                setData("title", e.target.value)
+                                            }
+                                        />
+                                        <span className="text-red-600">
+                                            {errors.title}
+                                        </span>
+                                    </div>
+                                    <div className="mb-0">
+                                        <label className="">Content</label>
+                                        <textarea
+                                            type="text"
+                                            className="w-full rounded"
+                                            label="content"
+                                            name="content"
+                                            errors={errors.content}
+                                            value={data.content}
+                                            onChange={(e) =>
+                                                setData(
+                                                    "content",
+                                                    e.target.value
+                                                )
+                                            }
+                                        />
+                                        <span className="text-red-600">
+                                            {errors.body}
+                                        </span>
+                                    </div>
+                                    <div className="mb-0">
+                                        <label className="">Start Date</label>
+                                        <textarea
+                                            type="text"
+                                            className="w-full rounded"
+                                            label="start-date"
+                                            name="start-date"
+                                            errors={errors.startDate}
+                                            value={data.startDate}
+                                            onChange={(e) =>
+                                                setData(
+                                                    "startDate",
+                                                    e.target.value
+                                                )
+                                            }
+                                        />
+                                        <span className="text-red-600">
+                                            {errors.body}
+                                        </span>
+                                    </div>
+                                    <div className="mb-0">
+                                        <label className="">End Date</label>
+                                        <textarea
+                                            type="text"
+                                            className="w-full rounded"
+                                            label="end-date"
+                                            name="end-date"
+                                            errors={errors.endDate}
+                                            value={data.endDate}
+                                            onChange={(e) =>
+                                                setData(
+                                                    "endDate",
+                                                    e.target.value
+                                                )
+                                            }
+                                        />
+                                        <span className="text-red-600">
+                                            {errors.body}
+                                        </span>
+                                    </div>
+                                    <div className="mb-0">
+                                        <label className="">Active</label>
+                                        <textarea
+                                            type="text"
+                                            className="w-full rounded"
+                                            label="active"
+                                            name="active"
+                                            errors={errors.active}
+                                            value={data.active}
+                                            onChange={(e) =>
+                                                setData(
+                                                    "active",
+                                                    e.target.value
+                                                )
+                                            }
+                                        />
+                                        <span className="text-red-600">
+                                            {errors.body}
+                                        </span>
+                                    </div>
+                                </div>
+                                <div className="mt-4">
+                                    <button
+                                        type="submit"
+                                        className="px-6 py-2 font-bold text-white bg-green-500 rounded"
+                                    >
+                                        Save
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Authenticated>
+    );
+}
